cec48b10c0d126ef8318fe0a01ce293a90e36d67
feliperyo <ryofeliperyo@gmail.com>
2024-04-11 22:03:38 -0300

    consumindo API

--- scripts.js
+++ scripts.js
@@ -1,5 +1,14 @@
+let chave = 'cebcd482eda57fa9a6714c1c2ba91885'
+
+async function searchCity() {
+    let dados = await fetch('https://api.openweathermap.org/data/2.5/weather?q=londres&appid=cebcd482eda57fa9a6714c1c2ba91885'
+    ).then(response => response.json())
+
+    console.log(dados)
+}
+
 function searchClick() {
     let input = document.querySelector('.input').value
 
-    console.log(input)
+    searchCity()
 }
