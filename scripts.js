3bf68b596ef3270a4379b78034fac5edca5a50d5
feliperyo <ryofeliperyo@gmail.com>
2024-04-15 11:01:16 -0300

    finalizando projeto

--- scripts.js
+++ scripts.js
@@ -1,14 +1,24 @@
 let chave = 'cebcd482eda57fa9a6714c1c2ba91885'
 
-async function searchCity() {
-    let dados = await fetch('https://api.openweathermap.org/data/2.5/weather?q=londres&appid=cebcd482eda57fa9a6714c1c2ba91885'
-    ).then(response => response.json())
+function showInfos(dados) {
+    document.querySelector('.city').innerHTML = `Tempo em ${dados.name}`
+    document.querySelector('.temp').innerHTML = Math.floor(dados.main.temp) + 'ºC'
+    document.querySelector('.icon').src = `https:openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    document.querySelector('.umidade').innerHTML = `Umidade: ${dados.main.humidity}%`
+    document.querySelector('.icons').style.visibility = 'hidden'
+}
 
+async function searchCity(input) {
+    let dados = await fetch('https://api.openweathermap.org/data/2.5/weather?q='
+        + input +
+        '&appid=cebcd482eda57fa9a6714c1c2ba91885&units=metric'
+    ).then(response => response.json())
     console.log(dados)
+    showInfos(dados)
 }
 
 function searchClick() {
     let input = document.querySelector('.input').value
 
-    searchCity()
+    searchCity(input)
 }
