a005327269580fcb4549e3facdc4aa1a32844298
feliperyo <ryofeliperyo@gmail.com>
2024-04-22 19:48:30 -0300

    ajuste API

--- scripts.js
+++ scripts.js
@@ -3,7 +3,8 @@ let chave = 'cebcd482eda57fa9a6714c1c2ba91885'
 function showInfos(dados) {
     document.querySelector('.city').innerHTML = `Tempo em ${dados.name}`
     document.querySelector('.temp').innerHTML = Math.floor(dados.main.temp) + 'ºC'
-    document.querySelector('.icon').src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+    document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/' + dados.weather[0].icon + '.png'
+    document.querySelector('.description').innerHTML = dados.weather[0].description
     document.querySelector('.umidade').innerHTML = `Umidade: ${dados.main.humidity}%`
     document.querySelector('.icons').style.visibility = 'hidden'
 }
@@ -11,7 +12,7 @@ function showInfos(dados) {
 async function searchCity(input) {
     let dados = await fetch('https://api.openweathermap.org/data/2.5/weather?q='
         + input +
-        '&appid=cebcd482eda57fa9a6714c1c2ba91885&units=metric'
+        '&appid=cebcd482eda57fa9a6714c1c2ba91885' + '&units=metric' + '&lang=pt_br'
     ).then(response => response.json())
     console.log(dados)
     showInfos(dados)
